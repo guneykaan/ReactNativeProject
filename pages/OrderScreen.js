294028cecec3867c49a8ac90d83f6e1ed615420e
Guneyhan Kaan <guneyhankaankursat@gmail.com>
2021-05-26 17:09:09 +0300

    OrderScreen Implementation Completed

--- pages/OrderScreen.js
+++ pages/OrderScreen.js
@@ -1,10 +1,53 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import baseManager from '../BaseService';
+import { ListItem, Icon } from 'react-native-elements';
 
 export default function OrderScreen() {
+    const [orders, setorders] = useState([]);
+
+    useEffect(() => {
+        fillData();
+    }, []);
+
+    const fillData = () => {
+        baseManager.get('api/orders')
+            .then((data) => {
+                setorders(data);
+            })
+    }
+
+    const deleteOrder = (id) => {
+        baseManager.delete('api/orders', id)
+            .then((data) => {
+                fillData();
+            })
+    }
+
     return (
         <View>
-            <Text></Text>
+            {
+                orders.map((order, index) => {
+                    return (
+                        <ListItem key={index}>
+                            <ListItem.Content>
+                                <ListItem.Title>{order.shipName}</ListItem.Title>
+                                <ListItem.Subtitle>{order.shipAddress.country}</ListItem.Subtitle>
+                                <ListItem.Subtitle>{order.shipAddress.city}</ListItem.Subtitle>
+                                <ListItem.Subtitle>{order.orderDate}</ListItem.Subtitle>
+                            </ListItem.Content>
+                            <Icon style={{justifyContent: 'flex-end'}}
+                                name='delete' onPress={() => deleteOrder(order.id)} />
+                        </ListItem>
+                    )
+                })
+            }
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    iconStyle: {
+        justifyContent: 'flex-end'
+    },
+  });
