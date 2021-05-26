eca12e8152ce560bfe4033d70566eedcf7199e62
Guneyhan Kaan <guneyhankaankursat@gmail.com>
2021-05-26 17:13:06 +0300

    CategoryScreenn Implementation Completed

--- pages/CategoryScreen.js
+++ pages/CategoryScreen.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import baseManager from '../BaseService';
+import { ListItem, Icon } from 'react-native-elements';
 
 export default function CategoryScreen() {
+
+    const [categories, setcategories] = useState([]);
+
+    useEffect(() => {
+        fillData();
+    }, []);
+
+    const fillData = () => {
+        baseManager.get('api/categories')
+            .then((data) => {
+                setcategories(data);
+            })
+    }
+
+    const deleteCategory = (id) => {
+        baseManager.delete('api/categories', id)
+            .then((data) => {
+                fillData();
+            })
+    }
+
+    const updateCategory = (id) =>{
+        console.log("update category cliclked!");
+    }
+
     return (
         <View>
-            <Text></Text>
-        </View>
+        {
+            categories.map((category, index) => {
+                return (
+                    <ListItem key={index}>
+                        <ListItem.Content>
+                            <ListItem.Title>{category.name}</ListItem.Title>
+                            <ListItem.Subtitle>{category.description}</ListItem.Subtitle>
+                        </ListItem.Content>
+                        <Icon style={{ justifyContent: 'flex-end' }}
+                            name='delete' onPress={() => deleteCategory(category.id)} />
+                        <Icon style={{ justifyContent: 'flex-end' }}
+                            name='update' onPress={() => updateCategory(category.id)} />
+                    </ListItem>
+                )
+            })
+        }
+    </View>
     )
 }
